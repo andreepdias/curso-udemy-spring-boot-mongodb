1b8d76a5a04f6a4b9e183df32a47c78f47a8f858
andreepdias <andre.dias@msn.com>
2020-10-25 18:35:18 -0300

    Atualização de User com PUT

--- src/main/java/udemy/resources/UserResource.java
+++ src/main/java/udemy/resources/UserResource.java
@@ -45,4 +45,12 @@ public class UserResource {
         service.delete(id);
         return ResponseEntity.noContent().build();
     }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Void> delete(@RequestBody UserDTO userDTO, @PathVariable String id){
+        User obj = service.fromDTO(userDTO);
+        obj.setId(id);
+        obj = service.update(obj);
+        return ResponseEntity.noContent().build();
+    }
 }
